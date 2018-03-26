27f8a6be0c92068beb9f93ac906f96907a4c02dd
Pernilla Qvist <pernilla.susanna@gmail.com>
2018-03-26 10:29:40 +0200

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,44 @@
+
+
+function navToggle() {
+    var  menu = document.getElementById("nav");
+    if (menu.className === "topnav") {
+        menu.className += "topnav.responsive";
+    } else {
+        menu.className = "topnav";
+    }
+}
+
+function closeMenu() {
+    var menu = document.getElementById("nav");
+    if (menu.className === "topnav.responsive") {
+        menu.className = "topnav";
+    } else {
+        menu.className = "topnav";
+    }
+}
+function clickFunctions(){
+    closeMenu();
+    move();
+}
+
+
+function move() {
+  let elem = document.getElementById("myBar");   
+  let elem2 = document.getElementById("myBar2");
+  let elem3 = document.getElementById("myBar3");
+  let elem4 = document.getElementById("myBar4");
+  var width = 1;
+  var id = setInterval(frame, 15);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem2.style.width = width + "%";
+      elem3.style.width = width + "%";
+      elem4.style.width = width + "%";
+    }
+  }
+}
